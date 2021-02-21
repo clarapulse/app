e493dc4f5fa026d3a0c6f69194d8c9a3f8e1580d
Eddie Zhang <ezhang7423@student.palomar.edu>
2021-02-21 01:58:05 -0800

    rm extra bracket

--- lib/Pages/chat/user_cards.dart
+++ lib/Pages/chat/user_cards.dart
@@ -16,15 +16,15 @@ class UserCardsWidget extends StatefulWidget {
 }
 
 Future<dynamic> fetchConnections() async {
-    final response = await http.get(Uri.https('clarapulsse.loca.lt', 'connections'),
-          headers: await getAuthToken());
-    if (response.statusCode == 200) {
-      dynamic post = jsonDecode(response.body).cast();
-      print(post);
-      return post;
-    } else {
-      throw Exception('Failed to load post');
-  }
+  final response = await http.get(
+      Uri.https('clarapulsse.loca.lt', 'connections'),
+      headers: await getAuthToken());
+  if (response.statusCode == 200) {
+    dynamic post = jsonDecode(response.body).cast();
+    print(post);
+    return post;
+  } else {
+    throw Exception('Failed to load post');
   }
 }
 
