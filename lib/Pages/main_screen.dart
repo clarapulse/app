0165c7c6a3e76057ca08f38ef451e0d9c6a791a8
Tanay Komarlu <tkomarlu@ucsb.edu>
2021-02-21 07:39:15 -0800

    profile page

--- lib/Pages/main_screen.dart
+++ lib/Pages/main_screen.dart
@@ -19,16 +19,68 @@ class HomeScreenWidget extends StatefulWidget {
 }
 
 class HomeScreenWidgetState extends State<HomeScreenWidget> {
+  User user = FirebaseAuth.instance.currentUser;
   static const String _title = 'ClaraPulse';
   final FirebaseMessaging _fcm = FirebaseMessaging();
   int _selectedIndex = 2;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   final List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Profile',
-      style: optionStyle,
-    ),
+    Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.blue[100], Colors.blue[400]],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                  user.photoURL,
+                ),
+                radius: 60,
+                backgroundColor: Colors.transparent,
+              ),
+              SizedBox(height: 40),
+              Text(
+                'NAME',
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54),
+              ),
+              Text(
+                user.displayName,
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'EMAIL',
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54),
+              ),
+              Text(
+                user.email,
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 40)
+            ],
+          ),
+        ),
+      ),
     ConnectContainerWidget(),
     UserCardsWidget(),
     Text(
